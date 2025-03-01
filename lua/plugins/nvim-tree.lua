4c90ce78f46d51a7dbb05960ad2bf459aeb13c6f
David Afonso <davafons@gmail.com>
2025-03-02 00:44:30 +0900

    Also for startup

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -13,16 +13,6 @@ return {
 
     -- Open the tree on load
     local function open_nvim_tree(data)
-      -- buffer is a real file on the disk
-      local real_file = vim.fn.filereadable(data.file) == 1
-
-      -- buffer is a [No Name]
-      local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
-
-      if not real_file and not no_name then
-        return
-      end
-
       -- open the tree, find the file but don't focus it
       require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
     end
